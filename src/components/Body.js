085c710080cfb89c90e98ddb0821a9fe13ec291b
Harish <harishbapkar@gmail.com>
2024-06-16 17:38:07 +0530

    filter fix

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,22 +2,22 @@ import ResturentCard, { withPromoted } from "./ResturentCard";
 import ShimerUI from "./ShimerUI";
 import { RESTURAENT_LIST_URL } from "../utils/constants";
 import { Link } from "react-router-dom";
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import useOnlineStatus from "../utils/useOnlineStatus";
 import UserContext from "../utils/UserContext";
 
 const Boday = () => {
   const [listOfResturent, setListOfResturent] = useState([]);
-  const [filterResturent, setFilterResturen] = useState([]);
   const [isError, setIsError] = useState(false);
 
-  const [searchText, setSearchText] = useState("type");
+  const [searchText, setSearchText] = useState("");
 
   const onlineStatus = useOnlineStatus();
 
   const ResturentCardPromoted = withPromoted(ResturentCard);
 
   const { loggedInUser, setUserName } = useContext(UserContext);
+  let intailDataRef = useRef(null);
 
   useEffect(() => {
     fetchData();
@@ -28,21 +28,22 @@ const Boday = () => {
       const data = await fetch(RESTURAENT_LIST_URL);
 
       const json = await data.json();
-      setFilterResturen(
-        json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle
-          ?.restaurants
-      );
       setListOfResturent(
-        json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle
+        json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle
           ?.restaurants
       );
+      intailDataRef.current =
+        json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
     } catch (err) {
       setIsError(true);
     }
   };
 
   if (isError) {
-    return <ShimerUI />;
+    return <h1>Something Went wrong</h1>;
+  }
+  if (listOfResturent.length === 0 || typeof listOfResturent === "undefined") {
+    return <h1>Resturent List is Empty</h1>;
   }
 
   if (onlineStatus === false) {
@@ -70,25 +71,33 @@ const Boday = () => {
                   .toLocaleLowerCase()
                   .includes(searchText.toLocaleLowerCase());
               });
-              setFilterResturen(searchResturent);
+              setListOfResturent(searchResturent);
             }}
           >
             🔎
           </button>
         </div>
         <button
-          className=" mx-1 px-1 bg-gray-200 rounded-md hover:bg-slate-300"
+          className=" hover:bg-slate-300"
           onClick={() => {
-            console.log("Top btn clicked");
             const topFilterdResturent = listOfResturent.filter(
-              (res) => res.info.avgRating > 4
+              (res) => res.info.avgRating > 4.5
             );
             setListOfResturent(topFilterdResturent);
           }}
         >
           Top Rated Resturents
         </button>
-        User:
+
+        <button
+          className=" mx-1"
+          onClick={() => {
+            setListOfResturent(intailDataRef.current);
+          }}
+        >
+          ❌Filters
+        </button>
+        {/* User:
         <input
           type="text"
           className=" border divide-solid to-black m-2 p-2"
@@ -96,11 +105,11 @@ const Boday = () => {
           onChange={(e) => {
             setUserName(e.target.value);
           }}
-        />
+        /> */}
       </div>
 
-      <div className=" flex flex-wrap">
-        {filterResturent.map((res) => (
+      <div className=" flex flex-wrap justify-center">
+        {listOfResturent.map((res) => (
           <Link key={res.info.id} to={"/restaurant/" + res.info.id}>
             {res.info.veg ? (
               <ResturentCardPromoted resdata={res} />
